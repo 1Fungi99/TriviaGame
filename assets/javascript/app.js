38ac5bc0a07256311d6109119cd5c0484365b607
Justin Cheng <ijustin17@gmail.com>
2019-10-12 13:51:28 -0500

    minor works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,36 @@
 $(document).ready(function () {
     var friends = {
-        questions=[
-            "Monica briefly dates billionaire Pete Becker. Which country does he take her for their first date?", 
-            "Rachel was popular in high school. Her prom date Chip ditched her for which girl at school?", 
-            "What’s the name of the 1950s-themed diner where Monica worked as a waitress?", 
-            "Janice gets married twice. What’s was her first husband’s name?", 
-            "Ursula was always mean! Which cartoon character was on Phoebe's thermos that Ursula threw under a bus?", 
+        questions: [
+            "Monica briefly dates billionaire Pete Becker. Which country does he take her for their first date?",
+            "Rachel was popular in high school. Her prom date Chip ditched her for which girl at school?",
+            "What’s the name of the 1950s-themed diner where Monica worked as a waitress?",
+            "Janice gets married twice. What’s was her first husband’s name?",
+            "Ursula was always mean! Which cartoon character was on Phoebe's thermos that Ursula threw under a bus?",
             "What is Joey's penguin's name?",
             "When Ross and Rachel were \“on a break,\" Ross slept with Chloe. Where does she work?",
             "Chandler’s mom had an interesting career and even more interesting love life. What’s her name?",
             "Monica and Chandler met on Thanksgiving in 1987. She pursued her career as a chef because Chandler complimented her on which dish?",
             "What fake name does Phoebe use when she wants to remain anonymous?"
         ],
-        answers1=['Rome', 'Italy', 'France', 'Canada'],
-        answers2=['Sally Roberts','Amy Welsh','Valerie Thompson'],
-        answers3=['Marilyn & Audrey','Twilight Galaxy','Moondance Diner'],
-        answers4=['Gary Litman','Sid Goralnik','Rob Bailystock'],
-        answers5=['Pebbles Flintstone','Yogi Bear','Judy Jetson','Bullwinkle'],
-        answers6=['Snowflake','Waddle','Hugsby'],
-        answers7=['Xerox','Microsoft','Domino\'s'],
-        answers8=['Priscilla Mae Galway','Nora Tyler Bing','Mary Jane Bing'],
-        answers9=['Green Bean Casserole','Meatloaf','Stuffing','Mac n Cheese'],
-        answers10=['Regina Phalange','Bonnie Riddle','Phyllis Halpert'],
+        answers1: ['Rome', 'Italy', 'France', 'Canada'],
+        answers2: ['Sally Roberts', 'Amy Welsh', 'Valerie Thompson'],
+        answers3: ['Marilyn & Audrey', 'Twilight Galaxy', 'Moondance Diner'],
+        answers4: ['Gary Litman', 'Sid Goralnik', 'Rob Bailystock'],
+        answers5: ['Pebbles Flintstone', 'Yogi Bear', 'Judy Jetson', 'Bullwinkle'],
+        answers6: ['Snowflake', 'Waddle', 'Hugsby'],
+        answers7: ['Xerox', 'Microsoft', 'Domino\'s'],
+        answers8: ['Priscilla Mae Galway', 'Nora Tyler Bing', 'Mary Jane Bing'],
+        answers9: ['Green Bean Casserole', 'Meatloaf', 'Stuffing', 'Mac n Cheese'],
+        answers10: ['Regina Phalange', 'Bonnie Riddle', 'Phyllis Halpert'],
     }
 
+    $(".start").on("click", function () {
+        console.log("fire");
+        $(".first").empty();
+setTimeout(timeOut, 30000)
+
+    });
+    function timeOut(){ console.log("it has been 30 seconds.")
+    }
 
 });
